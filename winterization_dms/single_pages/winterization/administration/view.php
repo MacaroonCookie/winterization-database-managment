ccbd63b3cbe6a47664dc8da2dbe29ceb265a52cb
Seth Cook <cooker52@gmail.com>
2013-09-29 20:58:45 -0400

    Moved administration from winterization/crew to winterization/

---
+++ winterization_dms/single_pages/winterization/administration/view.php
@@ -0,0 +1,5 @@
+<?php
+defined('C5_EXECUTE') or die(_("Access Denied."));
+?>
+<a href=<?=$this->url('/winterization/administration', 'projects');?>>Manipulate Projects</a><br/>
+<a href=<?=$this->url('/winterization/administration', 'residentjobs');?>>Manipulate Resident Jobs</a>
